b03ecf3fbb2c2ab4a10d39dd3c97cbfef82f390c
Benjamin Gregory Carlisle PhD <bgcarlisle@users.noreply.github.com>
2022-04-20 21:57:50 +0200

    Update reminders on export

--- data-raw/03-update-package.R
+++ data-raw/03-update-package.R
@@ -79,9 +79,10 @@ usethis::use_data(c19stoppedtrials, overwrite = TRUE)
 
 message(
     paste0(
-        "Don't forget to update the number of rows in the data set ",
-        "in `R/c19stoppedtrials.R` to ",
-        nrow(c19stoppedtrials),
+        "Don't forget to indicate the latest search date in ",
+        "`README.md` and `R/ctcovidstop-package.R`; then update the ",
+        "search date and the number of rows in the data set in ",
+        "`R/c19stoppedtrials.R` to ", nrow(c19stoppedtrials),
         ", then `document()`, `check()`, increment the version ",
         "number in `DESCRIPTION` and then update through git!"
     )
